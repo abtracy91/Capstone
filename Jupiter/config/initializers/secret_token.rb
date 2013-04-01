32df90d3bdce39110f59c3d6b1ba0eec13aad2b0
Alexander Tracy <alex8022@gmail.com>
2013-04-01 17:13:20 -0500

    Jupiter: rails new.

---
+++ Jupiter/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jupiter::Application.config.secret_token = '703f91761e871d35df18e67d276d8ca2056d772aab9996ac047543f981393de4efca9540300bba5865fe20920caed43a9b502e842adeb856ed9ae60176269444'
