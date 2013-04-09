be9083ec64cb8e7782309f91fac6dffbf733dc63
Alexander Tracy <alex8022@gmail.com>
2013-04-09 12:14:09 -0500

    Jupiter: Added 15, 60, 360 min tabs. Just lists for now.

--- Jupiter/app/controllers/display_controller.rb
+++ Jupiter/app/controllers/display_controller.rb
@@ -2,5 +2,11 @@ class DisplayController < ApplicationController
   def index
     @latest = Measurement.order("created_at").last
 
+    @last15 = Measurement.all(:conditions => ["created_at >= ?", DateTime.now - 15.minutes], :order => "created_at DESC")
+
+    @last60 = Measurement.all(:conditions => ["created_at >= ?", DateTime.now - 60.minutes], :order => "created_at DESC")
+
+    @last360 = Measurement.all(:conditions => ["created_at >= ?", DateTime.now - 360.minutes], :order => "created_at DESC")
+
   end
 end
